e83da2dc18a9b2a892b99b80bc8e229ebb562c7e
Cyber Punk Programmer <61289180+cyberpunkprogrammer@users.noreply.github.com>
2020-02-27 17:01:50 -0800

    Added VM.bat batch file.
    This file starts the virtual machine of your choice, in the example it starts a virtual machine labeled IDS.vmx
    It also sets the windows power mode to performance to prevent USB sleep and brings up the windows power options.
    YOU MUST CHANGE THIS LOCATION TO THE NAME OF YOUR VIRTUAL MACHINE
    Ideally, you would create a shortcut to this batch file after changing the path to the .vmx
    
    Tested on windows 10 and VMware Workstation Pro 15

---
+++ VM.bat
@@ -0,0 +1,11 @@
+@echo off
+
+powercfg -s 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
+echo msgbox "Power mode set to performance to prevent USB sleep." > "%temp%\popup.vbs"
+wscript.exe "%temp%\popup.vbs
+PATH "%PROGRAMFILES%\VMware\VMware Workstation\"
+
+rem Set your VM location .vmx file on the next line.
+
+START vmware.exe -x "%HOMEPATH%\Documents\Virtual Machines\IDS\IDS.vmx"
+%windir%\system32\control.exe /name Microsoft.PowerOptions
